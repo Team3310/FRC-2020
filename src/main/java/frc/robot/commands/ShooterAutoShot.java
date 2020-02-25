8cc2b5f0a694a899798fabe6e35e1bc5b3a2f333
Morgan Hissim <foxyroxy2283@gmail.com>
2020-02-24 20:04:42 -0600

    Group Commands Added

--- src/main/java/frc/robot/commands/ShooterAutoShot.java
+++ src/main/java/frc/robot/commands/ShooterAutoShot.java
@@ -5,21 +5,20 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Magazine;
 import frc.robot.subsystems.Shooter;
 
-public class ShooterFenderShot extends SequentialCommandGroup {
-
-    public ShooterFenderShot(Shooter shooter, Magazine magazine) {
+public class ShooterAutoShot extends SequentialCommandGroup {
+    public ShooterAutoShot(Shooter shooter, Magazine magazine) {
         // TODO: Add your sequential commands in the super() call, e.g.
         //           super(new FooCommand(), new BarCommand());
         addCommands(
 
                 // Set RPMs
-                new ShooterSetRPM(shooter, 2100, 2100, 2000), new MagazineSetRPM(magazine, 60),
+                new ShooterSetRPM(shooter, 3500, 3500, 3000), new MagazineSetRPM(magazine, 60),
 
                 // Current Limit
                 new MagazineSetRPMLimit(magazine, 60, 5),
 
                 // Hood Angle
-                new HoodSetAngle(shooter, 0)
+                new HoodSetAngle(shooter, 15)
 
                 // Turret
                 // Add Turret Command Here
